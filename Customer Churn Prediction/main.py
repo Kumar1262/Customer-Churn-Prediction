4afc7b8d0395ac96bef6f25d5a95f909b80c03ae
Kumar1262 <132981572+Kumar1262@users.noreply.github.com>
2024-04-08 21:08:09 +0530

    Add files via upload

---
+++ Customer Churn Prediction/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv('/Users/sagarsharma/Desktop/Slash Mark Projects/Customer-Churn-Prediction-main/Datasets/telecom_customer_churn.csv')
+
+print(df.head(5))
+
+df.columns 
